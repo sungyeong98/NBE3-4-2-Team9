847938b5be40f43ad10c08514f9486f68854168f
sungyeong98 <am25227903@gmail.com>
2025-01-24 17:57:39 +0900

    feat: 관리자 로그인 기능 구현

--- backend/src/main/java/com/backend/domain/user/controller/ApiV1UserController.java
+++ backend/src/main/java/com/backend/domain/user/controller/ApiV1UserController.java
@@ -1,10 +1,16 @@
 package com.backend.domain.user.controller;
 
+import com.backend.domain.user.dto.request.AdminLoginRequest;
 import com.backend.domain.user.dto.request.UserModifyProfileRequest;
 import com.backend.domain.user.dto.response.UserGetProfileResponse;
 import com.backend.domain.user.entity.SiteUser;
+import com.backend.domain.user.entity.UserRole;
+import com.backend.domain.user.service.AuthTokenService;
 import com.backend.domain.user.service.UserService;
+import com.backend.global.exception.GlobalErrorCode;
+import com.backend.global.exception.GlobalException;
 import com.backend.global.response.GenericResponse;
+import com.backend.global.rq.Rq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +21,31 @@ import org.springframework.web.bind.annotation.*;
 public class ApiV1UserController {
 
     private final UserService userService;
+    private final Rq rq;
+    private final AuthTokenService authTokenService;
+
+    @PostMapping("/adm/login")
+    @Transactional(readOnly = true)
+    public GenericResponse<Void> admLogin(
+            @RequestBody AdminLoginRequest req
+    ) {
+        SiteUser siteUser = userService.findByEmail(req.getEmail()).orElseThrow(
+                () -> new GlobalException(GlobalErrorCode.USER_NOT_FOUND)
+        );
+
+        if (!siteUser.getUserRole().equals(UserRole.ROLE_ADMIN)) {
+            throw new GlobalException(GlobalErrorCode.UNAUTHORIZATION_USER);
+        }
+
+        String accessToken = userService.genAccessToken(siteUser);
+        rq.setCookie("accessToken", accessToken);
+        rq.setCookie("apiKey", siteUser.getApiKey());
+
+        return GenericResponse.of(
+                true,
+                "200"
+        );
+    }
 
     @GetMapping("/users/{user_id}")
     @Transactional(readOnly = true)
