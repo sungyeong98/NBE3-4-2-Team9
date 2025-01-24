4cb22e7b8ccfef27376b7434e4611ee248e6e90a
Kim Dong O <kdo_1999@naver.com>
2025-01-24 12:36:22 +0900

    feat: GlobalException 추가

---
+++ backend/src/main/java/com/backend/global/exception/GlobalException.java
@@ -0,0 +1,33 @@
+package com.backend.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * GlobalException
+ * <p>공통으로 사용할 예외 클래스 입니다. <br><br>
+ * 사용 예시: </p>
+ * {@code
+ * throw new GlobalException(GlobalErrorCode.NOT_VALID);
+ * }
+ * @author Kim Dong O
+ */
+public class GlobalException extends RuntimeException {
+	private final GlobalErrorCode globalErrorCode;
+
+	/**
+	 * GlobalException 생성자 입니다.
+	 * @param globalErrorCode GlobalErrorCode 값
+	 */
+	public GlobalException(GlobalErrorCode globalErrorCode) {
+		super(globalErrorCode.getMessage());
+		this.globalErrorCode = globalErrorCode;
+	}
+
+	/**
+	 * 응답 HttpStatus를 반환하는 메서드 입니다.
+	 * @return {@link HttpStatus}
+	 */
+	public HttpStatus getStatus() {
+		return globalErrorCode.getHttpStatus();
+	}
+}
