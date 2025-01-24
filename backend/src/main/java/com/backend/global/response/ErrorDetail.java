fae16bfd6da442a4f288628e9ca8478549e2d504
Kim Dong O <kdo_1999@naver.com>
2025-01-24 15:09:49 +0900

    comment: 주석 수정

--- backend/src/main/java/com/backend/global/response/ErrorDetail.java
+++ backend/src/main/java/com/backend/global/response/ErrorDetail.java
@@ -3,6 +3,7 @@ package com.backend.global.response;
 /**
  * ErrorDetail
  * <p>@NotNull등과 같은 필드 에러 발생시 처리할 클래스</p>
+ *
  * @param field
  * @param reason
  * @author Kim Dong O
@@ -11,7 +12,8 @@ public record ErrorDetail(String field, String reason) {
 
 	/**
 	 * ErrorDetail 생성 팩토리 메서드
-	 * @param field 예외가 발생한 필드 이름
+	 *
+	 * @param field  예외가 발생한 필드 이름
 	 * @param reason 예외가 발생한 이유
 	 * @return {@link ErrorDetail}
 	 */
