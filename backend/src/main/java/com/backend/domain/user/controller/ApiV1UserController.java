68b8fe634b9b9c0f62e11b7e4370d4571b4f02d8
sungyeong98 <am25227903@gmail.com>
2025-01-27 14:23:11 +0900

    comment: 컨트롤러 메서드 주석 처리 - 테스트를 위해 작성했던 메서드들을 주석 처리함

--- backend/src/main/java/com/backend/domain/user/controller/ApiV1UserController.java
+++ backend/src/main/java/com/backend/domain/user/controller/ApiV1UserController.java
@@ -18,52 +18,52 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ApiV1UserController {
 
-    private final UserService userService;
-
-    @GetMapping("/users/{user_id}")
-    @Transactional(readOnly = true)
-    public GenericResponse<UserGetProfileResponse> getProfile(
-            @PathVariable Long user_id,
-            @AuthenticationPrincipal CustomUserDetails customUserDetails
-            ) {
-        SiteUser siteUser = userService.getUserById(user_id);
-
-        System.out.println("요청된 user_id: " + user_id);
-        System.out.println("토큰의 user: " + customUserDetails.getSiteUser().getId());
-
-        // TODO (유저 인증 코드 추가 예정)
-        if (customUserDetails == null) {
-            throw new GlobalException(GlobalErrorCode.USER_NOT_FOUND);
-        }
-
-        if (!user_id.equals(customUserDetails.getSiteUser().getId())) {
-            throw new GlobalException(GlobalErrorCode.UNAUTHORIZATION_USER);
-        }
-
-
-        return GenericResponse.of(
-                true,
-                "200",
-                new UserGetProfileResponse(siteUser)
-        );
-    }
-
-    @PatchMapping("/users/{user_id}")
-    @Transactional
-    public GenericResponse<Void> modifyProfile(
-            @PathVariable Long user_id,
-            @RequestBody UserModifyProfileRequest req
-    ) {
-        SiteUser siteUser = userService.getUserById(user_id);
-
-        // TODO (유저 인증 코드 추가 예정)
-
-        userService.modifyUser(siteUser, req);
-
-        return GenericResponse.of(
-                true,
-                "200"
-        );
-    }
+//    private final UserService userService;
+//
+//    @GetMapping("/users/{user_id}")
+//    @Transactional(readOnly = true)
+//    public GenericResponse<UserGetProfileResponse> getProfile(
+//            @PathVariable Long user_id,
+//            @AuthenticationPrincipal CustomUserDetails customUserDetails
+//            ) {
+//        SiteUser siteUser = userService.getUserById(user_id);
+//
+//        System.out.println("요청된 user_id: " + user_id);
+//        System.out.println("토큰의 user: " + customUserDetails.getSiteUser().getId());
+//
+//        // TODO (유저 인증 코드 추가 예정)
+//        if (customUserDetails == null) {
+//            throw new GlobalException(GlobalErrorCode.USER_NOT_FOUND);
+//        }
+//
+//        if (!user_id.equals(customUserDetails.getSiteUser().getId())) {
+//            throw new GlobalException(GlobalErrorCode.UNAUTHORIZATION_USER);
+//        }
+//
+//
+//        return GenericResponse.of(
+//                true,
+//                "200",
+//                new UserGetProfileResponse(siteUser)
+//        );
+//    }
+//
+//    @PatchMapping("/users/{user_id}")
+//    @Transactional
+//    public GenericResponse<Void> modifyProfile(
+//            @PathVariable Long user_id,
+//            @RequestBody UserModifyProfileRequest req
+//    ) {
+//        SiteUser siteUser = userService.getUserById(user_id);
+//
+//        // TODO (유저 인증 코드 추가 예정)
+//
+//        userService.modifyUser(siteUser, req);
+//
+//        return GenericResponse.of(
+//                true,
+//                "200"
+//        );
+//    }
 
 }
