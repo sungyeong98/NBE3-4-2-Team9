4f213eebbb4bb498933d60f333951c0bbe6c9cdc
zelly-log <dearmyjaei@gmail.com>
2025-01-27 16:07:28 +0900

    feat: Add Post Repository

---
+++ backend/src/main/java/com/backend/domain/board/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.backend.domain.board.repository;
+
+import com.backend.domain.board.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
