3b20a39e9f3faaec0b0e19caf35eaa23ec91fe51
Gonhub9 <ho_gok@naver.com>
2025-01-31 17:42:29 +0900

    feat: Category 추가 로직 작성 및 에러 코드 작성

--- backend/src/main/java/com/backend/domain/category/service/CategoryService.java
+++ backend/src/main/java/com/backend/domain/category/service/CategoryService.java
@@ -3,9 +3,16 @@ package com.backend.domain.category.service;
 import com.backend.domain.category.dto.response.CategoryResponse;
 import com.backend.domain.category.entity.Category;
 import com.backend.domain.category.repository.CategoryRepository;
+import com.backend.domain.user.entity.UserRole;
+import com.backend.global.exception.GlobalErrorCode;
+import com.backend.global.exception.GlobalException;
+import com.backend.global.security.custom.CustomUserDetails;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +25,37 @@ public class CategoryService {
     public List<CategoryResponse> categoryList() {
         List<Category> categoryList = categoryRepository.findAll();
         return mappingCategoryList(categoryList);
+    }
+
+    // 카테고리 추가 (관리자만 등록 가능)
+    public CategoryResponse createCategory(Category category) {
+        try {
+            // 인증된 사용자 정보 가져오기
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            // UserRole 역할 정보 가져오기
+            CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+            // Enum으로 반환
+            UserRole userRole = UserRole.fromString(userDetails.getRole());
+
+            // 관리자 권한 체크
+            if (!userRole.isAdmin()) {
+                throw new GlobalException(GlobalErrorCode.UNAUTHORIZATION_USER);
+            }
+
+            // 관리자일 경우 카테고리 등록 로직 실행
+            Category saveCategory = categoryRepository.save(category);
+            return mappingCategory(saveCategory);
+
+        } catch (DataAccessException e) {
+            // 데이터베이스 예외 처리
+            throw new GlobalException(GlobalErrorCode.DATABASE_ACCESS_ERROR);
+
+        } catch (Exception e) {
+            // 기타 예외 처리 (서버 오류로 예외 처리)
+            throw new GlobalException(GlobalErrorCode.INTERNAL_SERVER_ERROR);
+        }
     }
 
     // 카테고리 매핑
