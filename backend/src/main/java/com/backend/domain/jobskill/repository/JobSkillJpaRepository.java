71705966e818445159ab3a87422eff82dc73df34
Kim Dong O <kdo_1999@naver.com>
2025-02-03 16:34:40 +0900

    feat: JobSkillRepository 추가

---
+++ backend/src/main/java/com/backend/domain/jobskill/repository/JobSkillJpaRepository.java
@@ -0,0 +1,9 @@
+package com.backend.domain.jobskill.repository;
+
+import com.backend.domain.jobskill.entity.JobSkill;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobSkillJpaRepository extends JpaRepository<JobSkill, Long> {
+	Optional<JobSkill> findByCode(Integer code);
+}
---
+++ backend/src/main/java/com/backend/domain/jobskill/repository/JobSkillRepository.java
@@ -0,0 +1,34 @@
+package com.backend.domain.jobskill.repository;
+
+import com.backend.domain.jobskill.entity.JobSkill;
+import java.util.Optional;
+
+/**
+ * JobSkillRepository
+ * <p>JobSkill 리포지토리 입니다.</p>
+ *
+ * @author Kim Dong O
+ */
+public interface JobSkillRepository {
+
+	/**
+	 * @param id JobSkill id
+	 * @return {@link Optional<JobSkill>}
+	 * @implSpec Id 값으로 조회 메서드 입니다.
+	 */
+	Optional<JobSkill> findById(Long id);
+
+	/**
+	 * @param code JobSkill code
+	 * @return {@link Optional<JobSkill>}
+	 * @implSpec code 값으로 조회 메서드 입니다.
+	 */
+	Optional<JobSkill> findByCode(Integer code);
+
+	/**
+	 * @param jobSkill JobSkill 객체
+	 * @return {@link JobSkill}
+	 * @implSpec JobSkill 저장 메서드 입니다.
+	 */
+	JobSkill save(JobSkill jobSkill);
+}
