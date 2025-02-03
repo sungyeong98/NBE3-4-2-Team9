2c36fc4781b062bb22d0d9a4885bedc083e2bc14
sungyeong98 <am25227903@gmail.com>
2025-02-03 09:15:18 +0900

    refactor: 불필요한 코드 제거

--- backend/src/main/java/com/backend/global/security/handler/OAuth2LoginFailureHandler.java
+++ backend/src/main/java/com/backend/global/security/handler/OAuth2LoginFailureHandler.java
@@ -23,7 +23,6 @@ public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
     public void onAuthenticationFailure(
             HttpServletRequest request, HttpServletResponse response, AuthenticationException exception
     ) throws IOException {
-        System.out.println("OAuth2 로그인 실패 - 에러 메시지: " + exception.getMessage());
         exception.printStackTrace();
         
         AuthResponseUtil.failLogin(
