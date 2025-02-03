dea6653757eb84866b58182b6620db474ea673bb
Kim Dong O <kdo_1999@naver.com>
2025-02-03 16:34:25 +0900

    feat: JobPostingRepository 추가

---
+++ backend/src/main/java/com/backend/domain/jobposting/repository/JobPostingJpaRepository.java
@@ -0,0 +1,12 @@
+package com.backend.domain.jobposting.repository;
+
+import com.backend.domain.jobposting.entity.JobPosting;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface JobPostingJpaRepository extends JpaRepository<JobPosting, Long> {
+	@Query("select j from JobPosting j join fetch j.jobSkillList where j.id = :id")
+	Optional<JobPosting> findById(@Param("id") Long id);
+}
---
+++ backend/src/main/java/com/backend/domain/jobposting/repository/JobPostingRepository.java
@@ -0,0 +1,34 @@
+package com.backend.domain.jobposting.repository;
+
+import com.backend.domain.jobposting.entity.JobPosting;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * JobPostingRepository
+ * <p>JobPosting 리포지토리 입니다.</p>
+ *
+ * @author Kim Dong O
+ */
+public interface JobPostingRepository {
+
+	/**
+	 * @param id JobPosting id
+	 * @return {@link Optional<JobPosting>}
+	 * @implSpec Id 값으로 조회 메서드 입니다.
+	 */
+	Optional<JobPosting> findById(Long id);
+
+	/**
+	 * @param jobPosting JobPosting 객체
+	 * @return {@link JobPosting}
+	 * @implSpec JobPosting 저장 메서드 입니다.
+	 */
+	JobPosting save(JobPosting jobPosting);
+
+	/**
+	 * @return {@link List<JobPosting>}
+	 * @implSpec JobPosting 전체 조회 메서드 입니다.
+	 */
+	List<JobPosting> findAll();
+}
