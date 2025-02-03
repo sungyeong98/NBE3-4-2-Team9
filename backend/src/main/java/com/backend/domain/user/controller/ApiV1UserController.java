b67cdb272eecdd49531c51c311a43c02f9b31613
sungyeong98 <am25227903@gmail.com>
2025-02-03 10:11:48 +0900

    fix: 일반 사용자의 프로필 조회 실패 에러 수정

--- backend/src/main/java/com/backend/domain/user/controller/ApiV1UserController.java
+++ backend/src/main/java/com/backend/domain/user/controller/ApiV1UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class ApiV1UserController {
-
+//
 //    private final UserService userService;
 //
 //    @GetMapping("/users/{user_id}")
@@ -28,10 +28,6 @@ public class ApiV1UserController {
 //            ) {
 //        SiteUser siteUser = userService.getUserById(user_id);
 //
-//        System.out.println("요청된 user_id: " + user_id);
-//        System.out.println("토큰의 user: " + customUserDetails.getSiteUser().getId());
-//
-//        // TODO (유저 인증 코드 추가 예정)
 //        if (customUserDetails == null) {
 //            throw new GlobalException(GlobalErrorCode.USER_NOT_FOUND);
 //        }
@@ -39,7 +35,6 @@ public class ApiV1UserController {
 //        if (!user_id.equals(customUserDetails.getSiteUser().getId())) {
 //            throw new GlobalException(GlobalErrorCode.UNAUTHORIZATION_USER);
 //        }
-//
 //
 //        return GenericResponse.of(
 //                true,
