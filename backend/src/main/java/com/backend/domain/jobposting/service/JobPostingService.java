2f4ea5dac5a3b0c515bad03fd852ecb8ac28f92d
Kim Dong O <kdo_1999@naver.com>
2025-02-04 16:43:46 +0900

    feat: 페이징 동적 조회 기능 추가

---
+++ backend/src/main/java/com/backend/domain/jobposting/service/JobPostingService.java
@@ -0,0 +1,41 @@
+package com.backend.domain.jobposting.service;
+
+import com.backend.domain.jobposting.dto.JobPostingPageResponse;
+import com.backend.domain.jobposting.repository.JobPostingRepository;
+import com.backend.domain.jobposting.util.JobPostingSearchCondition;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * JobPostingService
+ * <p>채용 공고 서비스 입니다.</p>
+ *
+ * @author Kim Dong O
+ */
+@Service
+@RequiredArgsConstructor
+public class JobPostingService {
+
+	private final JobPostingRepository jobPostingRepository;
+
+	/**
+	 * 채용 공고 동적 페이징 조회 메서드 입니다.
+	 *
+	 * @param jobPostingSearchCondition 조회 조건 객체 {@link JobPostingSearchCondition}
+	 * @return {@link Page<JobPostingPageResponse>}
+	 */
+	public Page<JobPostingPageResponse> findAll(JobPostingSearchCondition jobPostingSearchCondition) {
+		int pageNum = jobPostingSearchCondition.pageNum() == null ?
+			0 : jobPostingSearchCondition.pageNum();
+
+		int pageSize = jobPostingSearchCondition.pageSize() == null ?
+			10 : jobPostingSearchCondition.pageSize();
+
+		Pageable pageable = PageRequest.of(pageNum, pageSize);
+
+		return jobPostingRepository.findAll(jobPostingSearchCondition, pageable);
+	}
+}
