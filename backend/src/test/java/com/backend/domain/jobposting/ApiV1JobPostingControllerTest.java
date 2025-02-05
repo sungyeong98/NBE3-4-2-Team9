493c86766de0483dbb6c80af54c387f53c4647cf
Kim Dong O <kdo_1999@naver.com>
2025-02-05 14:03:09 +0900

    test: Sql 어노테이션 추가

--- backend/src/test/java/com/backend/domain/jobposting/ApiV1JobPostingControllerTest.java
+++ backend/src/test/java/com/backend/domain/jobposting/ApiV1JobPostingControllerTest.java
@@ -5,34 +5,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.backend.domain.jobposting.entity.ExperienceLevel;
-import com.backend.domain.jobposting.entity.JobPosting;
-import com.backend.domain.jobposting.entity.JobPostingStatus;
-import com.backend.domain.jobposting.entity.RequireEducate;
-import com.backend.domain.jobposting.entity.Salary;
 import com.backend.domain.jobposting.repository.JobPostingRepository;
 import com.backend.global.exception.GlobalErrorCode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.ZonedDateTime;
-import java.util.stream.IntStream;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.annotation.Transactional;
 
 
 @SpringBootTest
+@Sql(scripts = {"/sql/init.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
+@Sql(scripts = {"/sql/delete.sql"}, executionPhase = ExecutionPhase.AFTER_TEST_CLASS)
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ApiV1JobPostingControllerTest {
 
 	@Autowired
@@ -43,101 +36,6 @@ public class ApiV1JobPostingControllerTest {
 
 	@Autowired
 	MockMvc mockMvc;
-
-	@BeforeAll
-	@Transactional
-	void setUp() {
-		ExperienceLevel experienceLevel1 = ExperienceLevel.builder()
-			.name("신입")
-			.code(1)
-			.build();
-
-		ExperienceLevel experienceLevel2 = ExperienceLevel.builder()
-			.name("경력")
-			.code(2)
-			.build();
-
-		Salary salaryCode9 = Salary.builder()
-			.code(9)
-			.name("2,600만원 이상")
-			.build();
-
-		Salary salaryCode22 = Salary.builder()
-			.code(22)
-			.name("1억원 이상")
-			.build();
-
-		Salary salaryCode99 = Salary.builder()
-			.code(99)
-			.name("면접 후 결정")
-			.build();
-
-		RequireEducate requireEducateCode0 = RequireEducate.builder()
-			.code(0)
-			.name("학력무관")
-			.build();
-
-		IntStream.range(0, 13).forEach((i) -> {
-			JobPosting jobPosting = JobPosting.builder()
-				.subject("testSubject")
-				.jobPostingStatus(JobPostingStatus.ACTIVE)
-				.postDate(ZonedDateTime.now())
-				.openDate(ZonedDateTime.now())
-				.closeDate(ZonedDateTime.now().plusDays(1))
-				.companyLink("test")
-				.companyName("test")
-				.experienceLevel(experienceLevel1)
-				.jobSkillList(null)
-				.requireEducate(requireEducateCode0)
-				.salary(salaryCode22)
-				.url("test")
-				.applyCnt((long) i)
-				.build();
-
-			jobPostingRepository.save(jobPosting);
-		});
-
-		IntStream.range(0, 13).forEach((i) -> {
-			JobPosting jobPosting = JobPosting.builder()
-				.subject("testSubject")
-				.jobPostingStatus(JobPostingStatus.ACTIVE)
-				.postDate(ZonedDateTime.now())
-				.openDate(ZonedDateTime.now())
-				.closeDate(ZonedDateTime.now().plusDays(1))
-				.companyLink("test")
-				.companyName("test")
-				.experienceLevel(experienceLevel2)
-				.jobSkillList(null)
-				.requireEducate(requireEducateCode0)
-				.salary(salaryCode99)
-				.url("test")
-				.applyCnt((long) i)
-				.build();
-
-			jobPostingRepository.save(jobPosting);
-		});
-
-		IntStream.range(0, 3).forEach((i) -> {
-			JobPosting jobPosting = JobPosting.builder()
-				.subject("testSubject")
-				.jobPostingStatus(JobPostingStatus.ACTIVE)
-				.postDate(ZonedDateTime.now())
-				.openDate(ZonedDateTime.now())
-				.closeDate(ZonedDateTime.now().plusDays(1))
-				.companyLink("test")
-				.companyName("test")
-				.experienceLevel(experienceLevel2)
-				.jobSkillList(null)
-				.requireEducate(requireEducateCode0)
-				.salary(salaryCode9)
-				.url("test")
-				.applyCnt((long) i)
-				.build();
-
-			jobPostingRepository.save(jobPosting);
-		});
-
-	}
 
 	@DisplayName("기본 페이징 조회 성공 테스트")
 	@Test
@@ -152,7 +50,7 @@ public class ApiV1JobPostingControllerTest {
 			.andExpect(jsonPath("$.code").value(200))
 			.andExpect(jsonPath("$.data.content.length()").value(10))
 			.andExpect(jsonPath("$.data.content").exists())
-			.andExpect(jsonPath("$.data.totalPages").value(3));
+			.andExpect(jsonPath("$.data.totalPages").value(2));
 	}
 
 	@DisplayName("페이징 경력 조건 신입 조회 성공 테스트")
@@ -167,10 +65,10 @@ public class ApiV1JobPostingControllerTest {
 		resultActions.andExpect(status().isOk())
 			.andExpect(jsonPath("$.success").value(true))
 			.andExpect(jsonPath("$.code").value(200))
-			.andExpect(jsonPath("$.data.content.length()").value(10))
+			.andExpect(jsonPath("$.data.content.length()").value(7))
 			.andExpect(jsonPath("$.data.content").exists())
 			.andExpect(jsonPath("$.data.content[2].experienceLevel.code").value(1))
-			.andExpect(jsonPath("$.data.totalPages").value(2));
+			.andExpect(jsonPath("$.data.totalPages").value(1));
 	}
 
 	@DisplayName("페이징 경력 조건 조회 성공 테스트")
@@ -185,10 +83,10 @@ public class ApiV1JobPostingControllerTest {
 		resultActions.andExpect(status().isOk())
 			.andExpect(jsonPath("$.success").value(true))
 			.andExpect(jsonPath("$.code").value(200))
-			.andExpect(jsonPath("$.data.content.length()").value(10))
+			.andExpect(jsonPath("$.data.content.length()").value(5))
 			.andExpect(jsonPath("$.data.content").exists())
 			.andExpect(jsonPath("$.data.content[2].experienceLevel.code").value(2))
-			.andExpect(jsonPath("$.data.totalPages").value(2));
+			.andExpect(jsonPath("$.data.totalPages").value(1));
 	}
 
 	@DisplayName("페이징 연봉 조건 조회 성공 테스트")
@@ -203,10 +101,10 @@ public class ApiV1JobPostingControllerTest {
 		resultActions.andExpect(status().isOk())
 			.andExpect(jsonPath("$.success").value(true))
 			.andExpect(jsonPath("$.code").value(200))
-			.andExpect(jsonPath("$.data.content.length()").value(10))
+			.andExpect(jsonPath("$.data.content.length()").value(7))
 			.andExpect(jsonPath("$.data.content").exists())
 			.andExpect(jsonPath("$.data.content[2].salary.code").value(99))
-			.andExpect(jsonPath("$.data.totalPages").value(2));
+			.andExpect(jsonPath("$.data.totalPages").value(1));
 	}
 
 	@DisplayName("페이징 조회 페이지 사이즈 음수 실패 테스트")
