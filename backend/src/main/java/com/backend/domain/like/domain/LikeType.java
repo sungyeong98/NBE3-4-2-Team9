1f1a6c4ce05e2b1433baf191e4e4daeb8095b2ab
Kim Dong O <kdo_1999@naver.com>
2025-02-05 20:01:22 +0900

    feat: LikeType 추가

---
+++ backend/src/main/java/com/backend/domain/like/domain/LikeType.java
@@ -0,0 +1,5 @@
+package com.backend.domain.like.domain;
+
+public enum LikeType {
+	JOB_POSTING, POST
+}
--- backend/src/main/java/com/backend/domain/like/entity/Like.java
+++ backend/src/main/java/com/backend/domain/like/entity/Like.java
@@ -1,8 +1,12 @@
 package com.backend.domain.like.entity;
 
 import com.backend.domain.jobposting.entity.JobPosting;
+import com.backend.domain.like.domain.LikeType;
 import com.backend.domain.user.entity.SiteUser;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -35,6 +39,10 @@ public class Like {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "site_user_id")
 	private SiteUser siteUser;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private LikeType likeType;
 
 	//TODO 추후 게시글 필드도 추가할 예정
 }
