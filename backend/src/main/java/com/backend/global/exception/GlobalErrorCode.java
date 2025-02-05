93067f53b79dc0b18b0a4952bde251c5645ca1f4
zelly-log <dearmyjaei@gmail.com>
2025-02-05 10:35:17 +0900

    feat: Add jobPosting GlobalErrorCode

--- backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java
+++ backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GlobalErrorCode {
 
+
     // 유저 도메인 에러 코드
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, 4001, "유저가 존재하지 않습니다."),
     UNAUTHENTICATION_USER(HttpStatus.UNAUTHORIZED, 4002, "인증되지 않은 사용자입니다."),
@@ -24,6 +25,7 @@ public enum GlobalErrorCode {
     KAKAO_LOGIN_FAIL(HttpStatus.BAD_REQUEST, 4005, "카카오 로그인에 실패하였습니다."),
     INVALID_JOB_SKILL(HttpStatus.BAD_REQUEST, 4006, "유효하지 않은 직무 기술입니다."),
     INVALID_TOKEN(HttpStatus.BAD_REQUEST, 4007, "유효하지 않은 토큰입니다."),
+
 
     // 게시글 도메인 에러 코드
     CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, 1001, "카테고리가 존재하지 않습니다."),
@@ -43,7 +45,7 @@ public enum GlobalErrorCode {
     NOT_VALID(HttpStatus.BAD_REQUEST, 5001, "요청하신 유효성 검증에 실패하였습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5002, "서버 내부 오류가 발생하였습니다.");
 
-	private final HttpStatus httpStatus;
-	private final int code;
-	private final String message;
+    private final HttpStatus httpStatus;
+    private final int code;
+    private final String message;
 }
