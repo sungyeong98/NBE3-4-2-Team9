f78ce1163e3097059d821f95608be0380f49f1c1
zelly-log <dearmyjaei@gmail.com>
2025-02-07 00:10:31 +0900

    refactor: repository 테스트 Springboottest -> DataJpaTest 변경

--- backend/src/test/java/com/backend/domain/post/PostRepositoryTest.java
+++ backend/src/test/java/com/backend/domain/post/PostRepositoryTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
@@ -22,7 +23,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+@DataJpaTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @Sql(scripts = {"/sql/init.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
