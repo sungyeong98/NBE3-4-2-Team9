cc75eb67a3584b0f4217b681b8aa8c6de96fee22
zelly-log <dearmyjaei@gmail.com>
2025-02-07 17:27:28 +0900

    feat: 게시글 수정 권한 없음 globalerrorcode 추가

--- backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java
+++ backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 /**
  * GlobalErrorCode
  * <p>예외 발생시 사용할 ErrorCode 입니다. <br>
- * 응답시 상태 코드, 커스텀 코드, 메세지를 정의합니다. <br>
- * 커스텀 코드는 각 도메인 별로 100 단위로 코드를 정의해주시면 됩니다. <br>
- * ex) Board - 100 -> 커스텀 코드 1001, 1002 순으로 정의하시면 됩니다.</p>
+ * 응답시 상태 코드, 커스텀 코드, 메세지를 정의합니다. <br> 커스텀 코드는 각 도메인 별로 100 단위로 코드를 정의해주시면 됩니다. <br> ex) Board - 100
+ * -> 커스텀 코드 1001, 1002 순으로 정의하시면 됩니다.</p>
+ *
  * @author Kim Dong O
  */
 @Getter
@@ -25,20 +25,20 @@ public enum GlobalErrorCode {
     INVALID_JOB_SKILL(HttpStatus.BAD_REQUEST, 4006, "유효하지 않은 직무 기술입니다."),
     INVALID_TOKEN(HttpStatus.BAD_REQUEST, 4007, "유효하지 않은 토큰입니다."),
 
-
     // 게시글 도메인 에러 코드
     CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, 1001, "카테고리가 존재하지 않습니다."),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, 1002, "게시글이 존재하지 않습니다."),
     POST_DELETE_FORBIDDEN(HttpStatus.FORBIDDEN, 1003, "게시글 삭제 권한이 없습니다."),
+    POST_UPDATE_FORBIDDEN(HttpStatus.FORBIDDEN, 1004, "게시글 업데이트 권한이 없습니다."),
 
-	// 카테고리 도메인 에러 코드
-	DUPLICATED_CATEGORY_NAME(HttpStatus.BAD_REQUEST, 3001, "카테고리 이름이 이미 존재합니다."),
-	INVALID_CATEGORY_NAME(HttpStatus.BAD_REQUEST, 3002, "카테고리 이름이 유효하지 않습니다."),
-	NAME_MISMATCH(HttpStatus.BAD_REQUEST, 3004, "카테고리 이름이 일치하지 않습니다."),
+    // 카테고리 도메인 에러 코드
+    DUPLICATED_CATEGORY_NAME(HttpStatus.BAD_REQUEST, 3001, "카테고리 이름이 이미 존재합니다."),
+    INVALID_CATEGORY_NAME(HttpStatus.BAD_REQUEST, 3002, "카테고리 이름이 유효하지 않습니다."),
+    NAME_MISMATCH(HttpStatus.BAD_REQUEST, 3004, "카테고리 이름이 일치하지 않습니다."),
 
-	// 관심 에러 코드 7001 ~
-	ALREADY_LIKE(HttpStatus.BAD_REQUEST, 7001, "이미 관심을 추가하였습니다."),
-	NOT_SUPPORT_TYPE(HttpStatus.BAD_REQUEST, 7002, "지원하지 않는 관심 타입 입니다."),
+    // 관심 에러 코드 7001 ~
+    ALREADY_LIKE(HttpStatus.BAD_REQUEST, 7001, "이미 관심을 추가하였습니다."),
+    NOT_SUPPORT_TYPE(HttpStatus.BAD_REQUEST, 7002, "지원하지 않는 관심 타입 입니다."),
 
     // 웹 소켓 에러 코드
     EXCEPTION_IN_WEBSOCKET(HttpStatus.UNAUTHORIZED, 6001, "웹 소켓 연결 중에 예외가 발생하였습니다."),
@@ -47,16 +47,16 @@ public enum GlobalErrorCode {
     JOB_POSTING_NOT_FOUND(HttpStatus.NOT_FOUND, 2001, "채용 정보가 존재하지 않습니다."),
     JOB_POSTING_REQUIRED(HttpStatus.BAD_REQUEST, 2002, "채용 정보가 필요합니다."),
 
-	//공통 서버 에러 코드 500
-	NOT_VALID(HttpStatus.BAD_REQUEST, 5001, "요청하신 유효성 검증에 실패하였습니다."),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5002, "서버 내부 오류가 발생하였습니다."),
+    //공통 서버 에러 코드 500
+    NOT_VALID(HttpStatus.BAD_REQUEST, 5001, "요청하신 유효성 검증에 실패하였습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5002, "서버 내부 오류가 발생하였습니다."),
 
     // JWT 관련 에러 코드
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, 7001, "토큰이 만료되었습니다."),
     TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, 7002, "토큰이 존재하지 않습니다.");
 
 
-	private final HttpStatus httpStatus;
-	private final int code;
-	private final String message;
+    private final HttpStatus httpStatus;
+    private final int code;
+    private final String message;
 }
