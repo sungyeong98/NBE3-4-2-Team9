a0405f40e04bc40f02bc031daf9018354e5e89a5
zelly-log <dearmyjaei@gmail.com>
2025-02-07 10:44:33 +0900

    refactor: category findByNmae 메서드 누락 추가

--- backend/src/main/java/com/backend/domain/category/repository/CategoryRepository.java
+++ backend/src/main/java/com/backend/domain/category/repository/CategoryRepository.java
@@ -1,10 +1,13 @@
 package com.backend.domain.category.repository;
 
 import com.backend.domain.category.entity.Category;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     // 중복 검사
     boolean existsByName(String name);
+
+    List<Category> findByName(String name);
 }
