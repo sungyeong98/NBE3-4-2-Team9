9cfafdf68e4602da6769d90ff77410f7cbcda680
Hyeonsuk <vkdnjdjxor@naver.com>
2025-02-07 14:42:30 +0900

    refactor: 채팅 페이징 조회 Json 직열화 해결

--- backend/src/main/java/com/backend/domain/chat/dto/response/ChatsInPost.java
+++ backend/src/main/java/com/backend/domain/chat/dto/response/ChatsInPost.java
@@ -1,8 +1,24 @@
 package com.backend.domain.chat.dto.response;
 
+import java.util.List;
 import org.springframework.data.domain.Page;
 
 public record ChatsInPost(
-        Page<ChatResponse> chats
+        List<ChatResponse> content,
+        int pageNumber,
+        int pageSize,
+        long totalElements,
+        int totalPages,
+        boolean last
 ) {
+    public ChatsInPost(Page<ChatResponse> page) {
+        this(
+                page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isLast()
+        );
+    }
 }
