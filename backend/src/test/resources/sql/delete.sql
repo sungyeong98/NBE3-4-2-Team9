7db25ad1cc8a157f384fe483ebfaa05cf1f71804
Hyeonsuk <vkdnjdjxor@naver.com>
2025-02-08 17:47:00 +0900

    feat: sql에 데이터 추가

--- backend/src/test/resources/sql/delete.sql
+++ backend/src/test/resources/sql/delete.sql
@@ -1,5 +1,6 @@
 SET REFERENTIAL_INTEGRITY FALSE;
 
+TRUNCATE TABLE recruitment_user;
 TRUNCATE TABLE user_job_skill;
 TRUNCATE TABLE post;
 TRUNCATE TABLE job_posting;
--- backend/src/test/resources/sql/init.sql
+++ backend/src/test/resources/sql/init.sql
@@ -61,6 +61,16 @@ VALUES (1, 'JAVA'),
        (2, 'C'),
        (3, 'PYTHON');
 
+-- RecruitmentUser 데이터 생성
+
+INSERT INTO recruitment_user (created_at, modified_at, post_id, site_user_id, status)
+VALUES
+    (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 1, 'APPLIED'),  -- 지원자 1이 게시글 1에 지원
+    (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 2, 'ACCEPTED'), -- 지원자 2가 게시글 1에 승인
+    (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2, 3, 'REJECTED'), -- 지원자 3이 게시글 2에 거절
+    (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2, 1, 'APPLIED');  -- 지원자 1이 게시글 2에 다시 지원
+
+
 -- Chat 데이터 생성
 INSERT INTO chat (created_at, modified_at, post_id, user_id, content, type)
 VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 1, 'content0', 'CHAT'),
