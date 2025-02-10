bd6475b54a3d677383fa459c1afbe5c862ae8bc0
Hyeonsuk <vkdnjdjxor@naver.com>
2025-02-10 09:23:20 +0900

    refactor: api Http 메서드 post-> patch 변경

--- backend/src/main/java/com/backend/domain/recruitmentUser/controller/ApiV1RecruitmentAuthorController.java
+++ backend/src/main/java/com/backend/domain/recruitmentUser/controller/ApiV1RecruitmentAuthorController.java
@@ -1,24 +1,26 @@
 package com.backend.domain.recruitmentUser.controller;
 
-import com.backend.domain.recruitmentUser.dto.request.AuthorRequest;
-import com.backend.domain.recruitmentUser.dto.response.RecruitmentUserPageResponse;
-import com.backend.domain.recruitmentUser.service.RecruitmentAuthorService;
-import com.backend.global.response.GenericResponse;
-import com.backend.global.security.custom.CustomUserDetails;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.domain.recruitmentUser.dto.request.AuthorRequest;
+import com.backend.domain.recruitmentUser.dto.response.RecruitmentUserPageResponse;
+import com.backend.domain.recruitmentUser.service.RecruitmentAuthorService;
+import com.backend.global.response.GenericResponse;
+import com.backend.global.security.custom.CustomUserDetails;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,7 +37,7 @@ public class ApiV1RecruitmentAuthorController {
      * @param request     모집 승인 요청 (userId 포함)
      * @return 성공 응답 (200 OK)
      */
-    @PostMapping("/{postId}/accept")
+    @PatchMapping("/{postId}/accept")
     public GenericResponse<Void> approveRecruitment(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long postId,
@@ -54,7 +56,7 @@ public class ApiV1RecruitmentAuthorController {
      * @param request     모집 거절 요청 (userId 포함)
      * @return 성공 응답 (200 OK)
      */
-    @PostMapping("/{postId}/reject")
+    @PatchMapping("/{postId}/reject")
     public GenericResponse<Void> rejectRecruitment(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long postId,
