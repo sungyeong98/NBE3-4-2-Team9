e0792698ee4586d21fa3b910c2e45f97e3d83519
Kim Dong O <kdo_1999@naver.com>
2025-02-10 14:11:21 +0900

    comment: 관심 -> 추천으로 용어 변경

--- backend/src/main/java/com/backend/domain/jobposting/controller/ApiV1JobPostingController.java
+++ backend/src/main/java/com/backend/domain/jobposting/controller/ApiV1JobPostingController.java
@@ -1,18 +1,17 @@
 package com.backend.domain.jobposting.controller;
 
+import com.backend.domain.jobposting.dto.JobPostingDetailResponse;
 import com.backend.domain.jobposting.dto.JobPostingPageResponse;
 import com.backend.domain.jobposting.service.JobPostingService;
 import com.backend.domain.jobposting.util.JobPostingSearchCondition;
-import com.backend.domain.jobposting.dto.JobPostingDetailResponse;
-
 import com.backend.global.response.GenericResponse;
+import com.backend.global.security.custom.CustomUserDetails;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import jakarta.validation.Valid;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import com.backend.global.security.custom.CustomUserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,7 +64,7 @@ public class ApiV1JobPostingController {
 	}
 
 	/**
-	 * 관심 채용 공고 조회 메서드 입니다.
+	 * 추천 채용 공고 조회 메서드 입니다.
 	 *
 	 * @return {@link GenericResponse<Page<JobPostingPageResponse>>}
 	 */
