795dc9576126ad8e7f126c6070912917e35ed1ed
Hyeonsuk <vkdnjdjxor@naver.com>
2025-02-25 11:16:20 +0900

    feat: mapper(라이브러리) 클래스에서 converter로 변경

--- backend/src/main/java/com/backend/domain/chat/converter/ChatConverter.java
+++ backend/src/main/java/com/backend/domain/chat/converter/ChatConverter.java
@@ -3,16 +3,18 @@ package com.backend.domain.chat.converter;
 import com.backend.domain.chat.dto.request.ChatRequest;
 import com.backend.domain.chat.dto.response.ChatResponse;
 import com.backend.domain.chat.entity.Chat;
+import com.backend.domain.user.entity.SiteUser;
 
 public class ChatConverter {
 
 	/**
 	 *  ChatRequest → Chat 변환 (빌더 패턴)
 	 */
-	public static Chat toChat(ChatRequest chatRequest, Long postId) {
+	public static Chat toChat(ChatRequest chatRequest, String postId, SiteUser user){
 		return Chat.builder()
 			.postId(postId)
 			.userId(chatRequest.userId())
+			.username(user.getName())
 			.content(chatRequest.content())
 			.type(chatRequest.type())
 			.build();
@@ -21,11 +23,10 @@ public class ChatConverter {
 	/**
 	 *  Chat → ChatResponse 변환 (빌더 패턴)
 	 */
-	public static ChatResponse toChatResponse(Chat chat, String username, String profileImg) {
+	public static ChatResponse toChatResponse(Chat chat) {
 		return ChatResponse.builder()
 			.userId(chat.getUserId())
-			.username(username)
-			.profileImg(profileImg)
+			.username(chat.getUsername())
 			.content(chat.getContent())
 			.type(chat.getType())
 			.createdAt(chat.getCreatedAt())
