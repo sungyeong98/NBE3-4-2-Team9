01fcb6fe95593cd79185422f0fa59d283f6d8cbd
Kim Dong O <kdo_1999@naver.com>
2025-02-27 09:12:03 +0900

    feat: Redis hasKey, save 추가

--- backend/src/main/java/com/backend/global/redis/repository/RedisRepository.java
+++ backend/src/main/java/com/backend/global/redis/repository/RedisRepository.java
@@ -1,11 +1,9 @@
 package com.backend.global.redis.repository;
 
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
-import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -23,6 +21,14 @@ public class RedisRepository {
 
     public void remove(String key) {
         redisTemplate.delete(key);
+    }
+
+    public boolean hasKey(String key) {
+        return redisTemplate.hasKey(key); //Redis에 key가 존재 하는지
+    }
+
+    public void save(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value); //Redis에 key, value 저장
     }
 
 }
