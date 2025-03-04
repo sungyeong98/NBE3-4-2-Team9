432d98f95ab39125b24dbf347dcaed6897f43afd
Hyeonsuk <vkdnjdjxor@naver.com>
2025-03-04 10:36:14 +0900

    test: 몽고디비로 전환과정에서 테스트 주석처리

--- backend/src/test/java/com/backend/domain/chat/util/ChatObjectProvider.java
+++ backend/src/test/java/com/backend/domain/chat/util/ChatObjectProvider.java
@@ -1,29 +1,31 @@
-package com.backend.domain.chat.util;
-
-import com.backend.domain.chat.entity.Chat;
-import com.backend.domain.chat.entity.MessageType;
-import java.util.List;
-import java.util.stream.IntStream;
-
-public class ChatObjectProvider {
-
-    public static Chat getChat(Long postId, Long userId, String content) {
-        return Chat.builder()
-                .postId(postId)
-                .userId(userId)
-                .content(content)
-                .type(MessageType.CHAT)
-                .build();
-    }
-
-    public static List<Chat> getChatList(Long postId, Long userId) {
-
-        return IntStream.range(0, 10)
-                .mapToObj(i -> Chat.builder()
-                        .userId(userId)
-                        .postId(postId)
-                        .content("content" + i)
-                        .type(MessageType.CHAT)
-                        .build()).toList();
-    }
-}
+// package com.backend.domain.chat.util;
+//
+// import java.util.List;
+// import java.util.stream.IntStream;
+//
+// import com.backend.domain.chat.entity.Chat;
+// import com.backend.domain.chat.entity.MessageType;
+//
+// public class ChatObjectProvider {
+//
+//     public static Chat getChat(String postId, String userId, String username, String content) {
+//         return Chat.builder()
+//                 .postId(postId)
+//                 .userId(userId)
+//                 .username(username)
+//                 .content(content)
+//                 .type(MessageType.CHAT)
+//                 .build();
+//     }
+//
+//     public static List<Chat> getChatList(String postId, String userId) {
+//
+//         return IntStream.range(0, 10)
+//                 .mapToObj(i -> Chat.builder()
+//                         .userId(userId)
+//                         .postId(postId)
+//                         .content("content" + i)
+//                         .type(MessageType.CHAT)
+//                         .build()).toList();
+//     }
+// }
