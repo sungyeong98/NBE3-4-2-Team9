af3d9616159b1a6becc1c41c57fc5e0cf5279cd1
Hyeonsuk <vkdnjdjxor@naver.com>
2025-03-04 12:05:15 +0900

    feat: Merge Conflict중 유실된 코드 수정

--- backend/src/main/java/com/backend/domain/chat/controller/ApiV1ChatController.java
+++ backend/src/main/java/com/backend/domain/chat/controller/ApiV1ChatController.java
@@ -2,7 +2,6 @@ package com.backend.domain.chat.controller;
 
 import static org.springframework.http.MediaType.*;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
